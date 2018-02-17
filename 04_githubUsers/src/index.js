1b8e94500376f4733b4897a0c257f32725872fe9
balabukha <3909591@gmail.com>
2018-02-17 14:56:46 +0700

    dev_redux_04_2

--- 04_githubUsers/src/index.js
+++ 04_githubUsers/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+// import App from './containers/App';
+import Picker from './components/Picker';
 
-import store from './store/index';
+// import store from './store/index';
 import {Provider} from 'react-redux'
 
 function Index() {
 
-    return (
-        <Provider store= {store} >
-            <App />
-        </Provider>
-        )
+    // return (
+    //     <Provider store= {store} >
+    //         <App />
+    //     </Provider>
+    //     )
+    return(
+        <div>
+            <Picker />
+        </div>
+    )
 
 };
 
