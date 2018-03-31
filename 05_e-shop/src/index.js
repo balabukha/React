c8fbef932fce588a7f271b5ee1fc2fd362faf2e4
balabukha <3909591@gmail.com>
2018-03-31 22:20:57 +0300

    fin

--- 05_e-shop/src/index.js
+++ 05_e-shop/src/index.js
@@ -11,6 +11,7 @@ import reducers from 'reducers';
 import Layout from 'containers/Layout';
 import Phones from 'containers/Phones';
 import Phone from 'containers/Phone';
+import Basket from 'containers/Basket';
 
 // == CSS == //
 
@@ -30,6 +31,7 @@ ReactDOM.render(
                 <Route path="/categories/:id" component={Phones}/>
             </Route>
             <Route path="/phones/:id" component={Phone}/>
+            <Route path="/basket" component={Basket}/>
         </Router>
     </Provider>,
     document.getElementById('root')
