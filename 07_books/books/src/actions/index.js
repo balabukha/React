2bdb2c4605aa96134017b126612d05d1383b5ed6
balabukha <3909591@gmail.com>
2018-04-21 12:39:51 +0300

    finishing forgot password

--- 07_books/books/src/actions/index.js
+++ 07_books/books/src/actions/index.js
@@ -48,4 +48,6 @@ export const resetPasswordRequest = ({email}) => () =>
 
 export const validateToken = (token) => () => api.user.validateToken(token);
 
+export const resetPassword = (data) => () => api.user.resetPassword(data);
+
 
--- 07_books/books/src/components/pages/SignUpPage/index.jsx
+++ 07_books/books/src/components/pages/SignUpPage/index.jsx
@@ -1,30 +1,29 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import SignupForm from '../../forms/SignupForm'
-import { signup } from '../../../actions'
+import SignupForm from "../../forms/SignupForm";
+import { signup } from "../../../actions";
 
 class SignUpPage extends Component {
+  submit = data =>
+    this.props.signup(data).then(() => this.props.history.push("/dashboard"));
 
-    submit = ( data ) =>
-        this.props.signup(data).then( () => this.props.history.push('/dashboard') );
-
-    render(){
-        return(
-            <div>
-                <h3>Please register</h3>
-                <SignupForm submit={this.submit} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Please register</h3>
+        <SignupForm submit={this.submit} />
+      </div>
+    );
+  }
 }
 
 SignUpPage.propTypes = {
-    history: PropTypes.shape({
-        push: PropTypes.func.isRequired,
-    }).isRequired,
-    signup: PropTypes.func.isRequired
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  signup: PropTypes.func.isRequired
 };
 
-export default connect(null, { signup })(SignUpPage)
+export default connect(null, { signup })(SignUpPage);
